113ce051b8c823419f4f9fd7f2f3817b78807b2c
yejinie <smoytn3@naver.com>
2020-09-24 00:36:21 +0900

    java practice

---
+++ java/exercises/ch2practice4.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class ch2practice4 {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("정수 3개 입력>>");
+	int a = scanner.nextInt();
+	int b = scanner.nextInt();
+	int c = scanner.nextInt();
+	if(a>b) {
+		if (a>c) {
+			if (b>c)
+				System.out.println("중간 값은 " + b);
+			else
+				System.out.println(c);
+		}
+		else {
+			System.out.println(a);
+		}
+	}
+	else {
+		if (c>a) {
+			if (b>c)
+				System.out.println(c);
+			else
+				System.out.println(b);
+		}
+		else
+			System.out.println(a);
+	}
+	scanner.close();
+	}
+}
