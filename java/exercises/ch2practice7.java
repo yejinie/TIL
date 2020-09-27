107893e253dbfbae1e8dffe9b0ca3bc2e08dff92
yejinie <smoytn3@naver.com>
2020-09-27 17:09:31 +0900

    java practice

---
+++ java/exercises/ch2practice7.java
@@ -0,0 +1,16 @@
+package exercises;
+import java.util.Scanner;
+
+public class ch2practice7 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("점 (x,y)의 좌표를 입력하시오>>");
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		if(100<=x && x<=200 && 100<=y && y<=200)
+			System.out.println("("+x+","+y+")는 사각형 안에 있습니다.");
+		else
+			System.out.println("("+x+","+y+")는 사각형 안에 없습니다.");
+		scanner.close();	
+	}
+}
