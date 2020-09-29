fa459167fd4a7ad97a77715246b2515d519e1fb9
yejinie <smoytn3@naver.com>
2020-09-29 20:06:41 +0900

    java exercise

---
+++ java/exercises/ch3practice10.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class ch3practice10 {
+	public static void main(String[] args) {
+		int intArray [][] = new int [4][4];
+		int i,p,q;
+		for(int num = 0; num<10; num++) {
+			i = (int)(Math.random()*10 + 1);
+			p = (int)(Math.random()*4 + 1);
+			q = (int)(Math.random()*4 + 1);
+			if(intArray [p-1][q-1] == 0)
+				intArray [p-1][q-1] = i;
+			else
+				num--;
+		}
+		for(int a=0; a<4; a++) {
+			for(int b=0; b<4; b++)
+				System.out.print(intArray [a][b] + "\t");
+			System.out.println();
+		}
+	}	
+}
